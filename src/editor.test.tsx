05018d0065bea0870385288fd2664c499641b531
Onur Hakbilen <onur.hakbilen@sparebank1.no>
2023-09-27 09:18:25 +0200

    Cleanup

--- src/editor.test.tsx
+++ src/editor.test.tsx
@@ -2,17 +2,17 @@
 
 import { Editor } from 'slate'
 import { jsx } from 'slate-hyperscript'
-import { describe, it } from 'vitest'
+import { it } from 'vitest'
 
 import { BaseEditor } from 'slate'
 import { ReactEditor } from 'slate-react'
 
-type CustomElement = { type: 'paragraph'; id: string, children: CustomText[] }
+type CustomElement = { type: 'paragraph'; id: string; children: CustomText[] }
 type CustomText = { text: string; bold?: true }
 
 declare module 'slate' {
   interface CustomTypes {
-    Editor: BaseEditor & ReactEditor 
+    Editor: BaseEditor & ReactEditor
     Element: CustomElement
     Text: CustomText
   }
@@ -27,8 +27,6 @@ const input = (
   </editor>
 ) as any as Editor
 
-describe('blockIdPlugin', () => {
-  it('apply node id on element insertion', () => {
-    Editor.insertNode(input, { type: 'paragraph', id: '1', children: [] })
-  })
+it('insert paragraph', () => {
+  Editor.insertNode(input, { type: 'paragraph', id: '1', children: [{ text: '' }] })
 })
